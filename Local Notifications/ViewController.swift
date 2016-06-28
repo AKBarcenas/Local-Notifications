ae0cde4642d1cae4ee994812d7eca422aa9acb54
AKBarcenas <akbarcen@ucsd.edu>
2016-06-28 04:16:22 -0700

    Allow the app to send notifications to the user.

--- Local Notifications/ViewController.swift
+++ Local Notifications/ViewController.swift
@@ -20,6 +20,44 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    /*
+     * Function Name: registerLocal
+     * Parameters: sender - the button that called this method.
+     * Purpose: This method that requests the user to give the application the authorization
+     *   to send notifications.
+     * Return Value: None
+     */
+    
+    @IBAction func registerLocal(sender: AnyObject) {
+        let notificationSettings = UIUserNotificationSettings(forTypes: [.Alert, .Badge, .Sound], categories: nil)
+        UIApplication.sharedApplication().registerUserNotificationSettings(notificationSettings)
+    }
 
+    /*
+     * Function Name: scheduleLocal
+     * Parameters: sender - the button that called this method.
+     * Purpose: This method checks to see that the user has authorized the application to send
+     *   notifications. It then schedules a notification with a specific style.
+     * Return Value: None
+     */
+    
+    @IBAction func scheduleLocal(sender: AnyObject) {
+        guard let settings = UIApplication.sharedApplication().currentUserNotificationSettings() else { return }
+        
+        if settings.types == .None {
+            let ac = UIAlertController(title: "Can't schedule", message: "Either we don't have permission to schedule notifications, or we haven't asked yet.", preferredStyle: .Alert)
+            ac.addAction(UIAlertAction(title: "OK", style: .Default, handler: nil))
+            presentViewController(ac, animated: true, completion: nil)
+            return
+        }
+        
+        let notification = UILocalNotification()
+        notification.fireDate = NSDate(timeIntervalSinceNow: 5)
+        notification.alertBody = "Hey you! Yeah you! Swipe to unlock!"
+        notification.alertAction = "be awesome!"
+        notification.soundName = UILocalNotificationDefaultSoundName
+        notification.userInfo = ["CustomField1": "w00t"]
+        UIApplication.sharedApplication().scheduleLocalNotification(notification)
+    }
 }
 
